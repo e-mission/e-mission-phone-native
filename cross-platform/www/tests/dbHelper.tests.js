88f4cdc4e745dfd37f0e8322e9bb1f2dfee76774
K. Shankari <shankari@eecs.berkeley.edu>
2014-11-13 09:48:21 -0800

    Invoke the tests from a separate test file instead of hardcoding them into the index.html
    This doesn't work with phantom.js but it does work in the emulator.

--- cross-platform/www/tests/dbHelper.tests.js
+++ cross-platform/www/tests/dbHelper.tests.js
@@ -1,9 +1,13 @@
-QUnit.test ("hello test", function(assert) {
-    assert.ok(1 == "1", "Passed!");
-});
+var dbHelperTests = {
+    doAllTests: function() {
+        QUnit.test ("hello test", function(assert) {
+            assert.ok(1 == "1", "Passed!");
+        });
 
-QUnit.asyncTest ("testPragmaExample", function(assert) {
-    console.log(dbHelper);
-    dbHelper.pragmaExample();
-    assert.ok(1 == "1", "Passed!");
-});
+        QUnit.asyncTest ("testPragmaExample", function(assert) {
+            console.log(dbHelper);
+            dbHelper.pragmaExample();
+            assert.ok(1 == "1", "Passed!");
+        });
+    }
+}
