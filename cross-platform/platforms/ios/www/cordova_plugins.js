ebe108fb0a325767d3bfb7c3d32c8f289481dd6a
K. Shankari <shankari@eecs.berkeley.edu>
2014-11-20 21:34:29 -0800

    Check in files to support ionic
    We had already created a cordova project with a significant hybrid component,
    so creating a new ionic project from scratch seemed like it would be a pain.
    Instead, I created an ionic template and copied over the www files.
    
    Testing it on ios appeared to work

--- cross-platform/platforms/ios/www/cordova_plugins.js
+++ cross-platform/platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,43 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.brodysoft.sqlitePlugin": "1.0.3"
+    "com.brodysoft.sqlitePlugin": "1.0.3",
+    "org.apache.cordova.device": "0.2.12",
+    "org.apache.cordova.console": "0.2.11",
+    "com.ionic.keyboard": "1.0.3"
 }
 // BOTTOM OF METADATA
 });
