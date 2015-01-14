0ef8a116a0ada484102400e6c1f054a2395eea93
K. Shankari <shankari@eecs.berkeley.edu>
2015-01-13 23:47:12 -0800

    Commit the changes to use ionic + store the data as text instead of a CLOB
    This is just to clean up the branch so that we can start fresh with ionic

--- cross-platform/platforms/android/assets/www/tests/dbHelper.tests.js
+++ cross-platform/platforms/android/assets/www/tests/dbHelper.tests.js
@@ -4,10 +4,17 @@ var dbHelperTests = {
             assert.ok(1 == "1", "Passed!");
         });
 
-        QUnit.asyncTest ("testPragmaExample", function(assert) {
-            console.log(dbHelper);
-            dbHelper.pragmaExample();
-            assert.ok(1 == "1", "Passed!");
-        });
+        // QUnit.asyncTest ("testPragmaExample", function(assert) {
+        //     console.log(dbHelper);
+        //     dbHelper.pragmaExample();
+        //     assert.ok(1 == "1", "Passed!");
+        // });
+
+    	QUnit.asyncTest ("testTripSections", function(assert) {
+            console.log("Testing console logs");
+    	    console.log(tripSectionDbHelper);
+    	    tripSectionDbHelper.getUncommitedSections();
+    	    assert.ok(1 == "1", "Passed!");
+    	});
     }
 }
